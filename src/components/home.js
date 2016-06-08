34f34c9998799ae2c4affa5da055027be2267e0c
Carlado <mikael_adolfsson@live.se>
2016-06-08 16:51:13 +0200

    set up react-router

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ResultList from './resultlist';
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <ResultList />
+    )
+  }
+}
