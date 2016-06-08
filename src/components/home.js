cf48d6e5aa337e714fd74081b92c3aa1133ce02a
Carlado <mikael_adolfsson@live.se>
2016-06-08 18:46:59 +0200

    add search bar

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import ResultList from './resultlist';
+import SearchBar from './searchbar';
 
 
 export default class Home extends Component {
   render() {
     return (
-      <ResultList />
+      <div>
+        <SearchBar />
+      </div>
+
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {Router, browserHistory} from 'react-router';
 import reducers from './reducers';
+import routes from './routes';
+
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
