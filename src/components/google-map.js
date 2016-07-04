4a0b55412972555289f0fe40245a9fe3967a304d
Carlado <mikael_adolfsson@live.se>
2016-07-04 22:59:23 +0200

    fix map marker

--- src/components/google-map.js
+++ src/components/google-map.js
@@ -2,13 +2,21 @@ import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 import React, {Component} from 'react';
 
 export default function LocationMap(props) {
+
+
   return (
     <GoogleMapLoader
       containerElement={
         <div style={{height: '100%'}}/>
       }
       googleMapElement={
-        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}}>
+        {props.markers.map((marker, index) => {
+            return (
+              <Marker {...marker}/>
+            );
+          })}
+        </GoogleMap>
       }
     />
   )
--- src/components/info-box.js
+++ src/components/info-box.js
@@ -7,17 +7,26 @@ class InfoBox extends Component {
 
   render() {
       const details = this.props.details[0].response.venue;
-      const formattedUrl = details.url.replace(/.*?:\/\//g, "");
+      const formattedUrl = details.url ? details.url.replace(/.*?:\/\//g, "") : "";
       const lat = details.location.lat;
       const lon = details.location.lng;
       return (
         <div className="col-md-4">
-          <LocationMap lat={lat} lon={lon}/>
+        <div className="map-area">
+          <LocationMap lat={lat} lon={lon} markers={[{
+            position: {
+              lat: lat,
+              lng: lon,
+            },
+            key: details.id,
+            defaultAnimation: 2,
+          }]}/>
+        </div>
           {details.name && <p>{details.name}</p>}
           <p>{details.location.formattedAddress[0]}</p>
           <p>{details.location.formattedAddress[1]}</p>
           <p>{details.location.formattedAddress[2]}</p>
-          {details.hours.status && <p>{details.hours.status}</p>}
+          {details.hours && <p>{details.hours.status}</p>}
           {details.contact.formattedPhone && <p>{details.contact.formattedPhone}</p>}
           {details.url && <p><a href={details.url} target="_blank">{formattedUrl}</a></p>}
         </div>
