728a40ac7b73f70b7d2ee74e45ce89f7a19931e8
Carlado <mikael_adolfsson@live.se>
2016-09-28 17:49:23 +0200

    downgrade NameBox to stateless component

--- src/components/name-box.js
+++ src/components/name-box.js
@@ -1,32 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-class NameBox extends Component {
-  render() {
-    const details = this.props.details.response.venue;
-    return (
-      <div className="row name-box venue-content">
-        <div className="col-xs-8">
-          <h3>{details.name}</h3>
-        </div>
-        <div className="col-xs-4">
-          <div className="venue-page-rating">
-            {details.rating ?
-              <span>{details.rating}</span> :
-              <span>-</span>
-            }
-          </div>
-        </div>
+const NameBox = ({venueDetails}) => {
 
+  return (
+    <div className="row name-box venue-content">
+      <div className="col-xs-8">
+        <h3>{venueDetails.name}</h3>
       </div>
-    )
-  }
+      <div className="col-xs-4">
+        <div className="venue-page-rating">
+          {venueDetails.rating ?
+            <span>{venueDetails.rating}</span> :
+            <span>-</span>
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    details: state.details
-  }
-}
-
-export default connect(mapStateToProps)(NameBox);
+export default NameBox;
