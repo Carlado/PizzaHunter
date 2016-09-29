601b443ddf8b71ba8ff8959eb3120a95ddd49728
Carlado <mikael_adolfsson@live.se>
2016-09-29 19:22:52 +0200

    add redux devtools extension support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import {Router, browserHistory} from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
 import ReduxPromise from 'redux-promise';
 
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const initialState = {
+  venues: null,
+  details: null,
+  photos: null
+};
+
+let store = createStore(reducers, initialState, compose(applyMiddleware(ReduxPromise),
+window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.main'));
